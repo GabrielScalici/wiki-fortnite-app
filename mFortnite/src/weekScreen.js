7906ed6ad149d6a4425297c5458b0fad5a5feca8
GabrielScalici <gabrielhc.scalici@gmail.com>
2019-03-19 12:07:56 -0300

    telas principais criadas

---
+++ mFortnite/src/weekScreen.js
@@ -0,0 +1,34 @@
+/**
+    Aplicativo Missoes do Fortnite
+    Criado e Produzido por: Gabriel Scalici e Gustavo Scalici
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+
+type Props = {};
+export default class weekScreen extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+
+});
